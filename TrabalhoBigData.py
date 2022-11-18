2b6ac604d7fd1ff2c05ea957c2f1fc83b6b96a6d
diegoouu <diegogomesmsgalhaes@gmail.com>
2022-11-18 20:08:13 -0300

    vai

--- TrabalhoBigData.py
+++ TrabalhoBigData.py
@@ -1,6 +1,7 @@
 import pandas as pd
-import numpy as np
+import matplotlib as mpl
 import matplotlib.pyplot as plt 
+import numpy as np
 
 
 dataset = pd.read_csv(
@@ -8,21 +9,19 @@ dataset = pd.read_csv(
 )
 
 #print('comprimento da sepala')
-#indices de tipo inteiro
-#s1 = pd.Series([1, 3, 4, 5, np.nan, 7, 8])
-#s1 = pd.DataFrame(np.random.randn(6, 4), columns=list("ABCD"))
+
 dataset.head()
-#print(s1)
+
 print(dataset)
 print("Tipo de dado de cada coluna")
 mostra = dataset.dtypes 
 print(mostra)
-#s1 = df['Sepal length'].mean()
+
 print('mostra cada atributo das colunas')
 coluna = list(dataset.columns)
 print (coluna)
 
-#s1 = s1.style.highlight_max(color = 'lightgreen', axis = 0)
+
 print( '__Media das Colunas__')
 print('Media da coluna 1\n ', dataset['Sepallength'].mean())
 print('Media da coluna 2\n ', dataset['SepalWidth'].mean())
@@ -60,7 +59,6 @@ print('min da coluna 2:\n ',dataset['SepalWidth'].min())
 print('min da coluna 3:\n ',dataset['PetalLength'].min())
 print('min da coluna 4:\n ',dataset['PetalWidth'].min())
 
-#print('min da coluna 1:\n ',dataset['Sepallength'].min())
 print('______Tabela Odenada de Forma Ascendente_______')
 print(dataset.sort_values('Sepallength'))
 print('______Tabela Ordenada de Forma Descendente_______')
@@ -95,8 +93,6 @@ print(dataset[['PetalLength']] > dataset[['PetalLength']].mean())
 print('Coluna 4:') 
 print('Media:',dataset['PetalWidth'].mean()) 
 print(dataset[['PetalWidth']] > dataset[['PetalWidth']].mean())
-
-
 
 
 
